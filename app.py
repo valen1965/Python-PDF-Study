a161d4526715c1ae34af3b4a5f0b354ab452d30a
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-08-09 01:09:48 +0300

    Added rotated PDF file

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ import PyPDF2
 
 with open("first.pdf", "rb") as file:
     reader = PyPDF2.PdfFileReader(file)
-    print(reader.numPages)
+   # print(reader.numPages)
+    page = reader.getPage(0)
+    page.rotateClockwise(90)
+    writer = PyPDF2.PdfFileWriter()
+    writer.addPage(page)
+    with open("rotated.pdf", "wb") as output:
+        writer.write(output)
